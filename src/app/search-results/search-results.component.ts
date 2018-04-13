3cbdfa961a73df72551aa557726178639c2c919f
yagizozturk <yagizozturk@gmail.com>
2018-04-13 16:05:49 +0300

    deck structure draft

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -13,22 +13,31 @@ export class SearchResultsComponent implements OnInit {
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   results = [];
+  images = [];
+  news = [];
 
   ngOnInit() {
-    this.getKeyword();
+    this.getWebPageResults();
   }
 
-  getKeyword(): void {
+  getWebPageResults(): void {
     let keyword: string = '';
     keyword = this.route.snapshot.paramMap.get('keyword');
     console.log('search result page --> ' + keyword);
     this.getJSON().subscribe(data => {      
-      this.results = data;
-      console.log(this.results);
+      this.results = data.webPageResults;
+      this.images = data.imageResults;
+      this.news = data.newsResults;
     });
   }
 
   getJSON(): Observable<any>{
     return this.http.get('./assets/webSearchResults.json');
+  }
+
+  next() : void {
+    alert("test");
+    var el = document.getElementById("next");
+    //el.next();
   }
 }
